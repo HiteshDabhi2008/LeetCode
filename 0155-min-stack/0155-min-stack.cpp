56580e8c7774e618e84db510f001d2a2ce4c64aa
Hitesh Dabhi <dabhihiteshalpeshbhai94263@gmail.com>
2026-09-26 11:30:07 +0530

    Time: 54 ms (69.78%), Space: 151 MB (81.07%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -4,52 +4,36 @@ public:
     long long mini;
 
     MinStack() {
-        
     }
 
     void push(int val) {
-
-        // First element
         if (st.empty()) {
             st.push(val);
             mini = val;
         }
-
-        // Normal value
         else if (val >= mini) {
             st.push(val);
         }
-
-        // New minimum
         else {
-            // Store encoded value
             st.push(2LL * val - mini);
-
-            // Update minimum
             mini = val;
         }
     }
 
     void pop() {
-
+        if(st.empty()) return;
         long long x = st.top();
         st.pop();
-
-        // x is an encoded value
         if (x < mini) {
             mini = 2LL * mini - x;
         }
     }
 
     int top() {
-
         long long x = st.top();
-
-        // Encoded value means actual value is mini
         if (x < mini) {
             return mini;
         }
-
         return x;
     }
 
